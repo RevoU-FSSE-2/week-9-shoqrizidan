de758dc1de47a81cd2f10410dbd19898385d64be
Shoqri Zidan <87312670+Shoqriz@users.noreply.github.com>
2023-08-19 02:39:52 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -6,10 +6,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const db = mysql.createConnection({
-  host: 'localhost',
+  host: 'containers-us-west-139.railway.app',
+  port: '5623',
   user: 'root',
-  password: 'zd2108',
-  database: 'szmbanking'
+  password: 'SrvhG1cAuTZY2olSb6yg',
+  database: 'railway'
 });
 
 app.use(bodyParser.json());
@@ -23,95 +24,88 @@ app.get('/', (req, res) => {
   res.send('MBanking App API');
 });
 
+// Get user information with balance and total expenses
 // GET /user/:id
 app.get('/user/:id', (req, res) => {
-  const userId = req.params.id;
-
-  db.query('SELECT * FROM user WHERE id = ?', [userId], (error, userResults) => {
-    if (error) {
-      return res.status(500).json({ message: 'Internal server error' });
-    }
-    if (userResults.length === 0) {
-      return res.status(404).json({ message: 'User not found' });
-    }
-
-    const user = userResults[0];
-
-    db.query('SELECT SUM(amount) AS total_income, SUM(CASE WHEN type="expense" THEN amount ELSE 0 END) AS total_expenses FROM transactions WHERE user_id = ?', [userId], (error, transactionResults) => {
+    const userId = req.params.id;
+  
+    db.query('SELECT * FROM user WHERE id = ?', [userId], (error, userResults) => {
       if (error) {
         return res.status(500).json({ message: 'Internal server error' });
       }
-
-      const totalIncome = transactionResults[0].total_income || 0;
-      const totalExpenses = transactionResults[0].total_expenses || 0;
-      const balance = totalIncome - totalExpenses;
-
-      const response = {
-        id: user.id,
-        name: user.name,
-        address: user.address,
-        balance: balance,
-        total_expenses: totalExpenses
-      };
-
-      res.json(response);
+      if (userResults.length === 0) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+  
+      const user = userResults[0];
+  
+      db.query('SELECT SUM(amount) AS total_income, SUM(CASE WHEN type="expense" THEN amount ELSE 0 END) AS total_expenses FROM transactions WHERE user_id = ?', [userId], (error, transactionResults) => {
+        if (error) {
+          return res.status(500).json({ message: 'Internal server error' });
+        }
+  
+        const totalIncome = transactionResults[0].total_income || 0;
+        const totalExpenses = transactionResults[0].total_expenses || 0;
+        const balance = totalIncome - totalExpenses;
+  
+        const response = {
+          id: user.id,
+          name: user.name,
+          address: user.address,
+          balance: balance,
+          total_expenses: totalExpenses
+        };
+  
+        res.json(response);
+      });
     });
+  });
+
+// Create new transaction
+app.post('/transactions', (req, res) => {
+  const { user_id, type, amount } = req.body;
+  const sql = 'INSERT INTO transactions (user_id, type, amount) VALUES (?, ?, ?)';
+  db.query(sql, [user_id, type, amount], (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'An error occurred while adding the transaction' });
+    } else {
+      res.json({
+        message: 'Transaction added successfully',
+        id: result.insertId,
+      });
+    }
   });
 });
 
-// POST /transaction
-app.post('/transactions', (req, res) => {
-    const { type, amount, user_id } = req.body;
-    if (type !== 'income' && type !== 'expense') {
-      res.status(400).json({ message: 'Invalid transaction type' });
-      return;
+// Update transaction
+app.put('/transactions/:id', (req, res) => {
+  const transactionId = req.params.id;
+  const { type, amount } = req.body;
+  const sql = 'UPDATE transactions SET type = ?, amount = ? WHERE id = ?';
+  db.query(sql, [type, amount, transactionId], (err) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'An error occurred while updating the transaction' });
+    } else {
+      res.json({ message: 'Transaction updated successfully' });
     }
-    db.query('INSERT INTO transactions (user_id, type, amount) VALUES (?, ?, ?)', [user_id, type, amount], (error, results) => {
-      if (error) {
-        res.status(500).json({ message: 'Internal server error' });
-        return;
-      }
-      res.json({ id: results.insertId });
-    });
   });
-  
-  // PUT /transaction/:id
-  app.put('/transactions/:id', (req, res) => {
-    const transactionId = req.params.id;
-    const { type, amount, user_id } = req.body;
-    if (type !== 'income' && type !== 'expense') {
-      res.status(400).json({ message: 'Invalid transaction type' });
-      return;
+});
+
+// Delete transaction
+app.delete('/transactions/:id', (req, res) => {
+  const transactionId = req.params.id;
+  const sql = 'DELETE FROM transactions WHERE id = ?';
+  db.query(sql, [transactionId], (err) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'An error occurred while deleting the transaction' });
+    } else {
+      res.json({ message: 'Transaction deleted successfully' });
     }
-    db.query('UPDATE transactions SET type = ?, amount = ? WHERE id = ?', [type, amount, transactionId], (error, results) => {
-      if (error) {
-        res.status(500).json({ message: 'Internal server error' });
-        return;
-      }
-      if (results.affectedRows === 0) {
-        res.status(404).json({ message: 'Transaction not found' });
-        return;
-      }
-      res.json({ id: transactionId });
-    });
   });
-  
-  // DELETE /transactions/:id
-  app.delete('/transaction/:id', (req, res) => {
-    const transactionId = req.params.id;
-    db.query('DELETE FROM transactions WHERE id = ?', [transactionId], (error, results) => {
-      if (error) {
-        res.status(500).json({ message: 'Internal server error' });
-        return;
-      }
-      if (results.affectedRows === 0) {
-        res.status(404).json({ message: 'Transaction not found' });
-        return;
-      }
-      res.json({ id: transactionId });
-    });
-  });
-  
+});
 
 app.listen(port, () => {
   console.log(`Server berjalan di port ${port}`);
